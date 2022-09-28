44d6836e55083bf928607e8c552066bdcf872bcd
sunnypankaj2 <sunnypankaj2@iitkgp.ac.in>
2022-09-28 11:54:23 +0530

    Updated the queries

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ if(process.env.NODE_ENV !== 'production'){
 }
 
 const DATABASE_URL = process.env.DATABASE_URL,dburl='mongodb://localhost/reminder_database';
-const port = process.env.PORT;
+const PORT = process.env.PORT;
 
 mongoose.connect(DATABASE_URL||dburl, { useNewUrlParser: true, useUnifiedTopology: true}, err=>{
     console.log("connecting to database....");
@@ -24,22 +24,22 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.listen(port|3000, err=>{
+app.listen(PORT|3000, err=>{
     if(err){
         console.error(err);
     }else{
-        console.log("listening at port",port);
+        console.log("listening at port",PORT);
     }
 });
 
 
 app.get('/getAllReminders', async (req,res)=>{
     try{    
-        const allReminders = await Reminder.find({});    
+        const allReminders = await Reminder.find();    
         if(allReminders.length !==0 ){  
-            res.status(200).send(allReminders);    
+            res.status(201).send(allReminders);    
         }else{   
-            res.send({message: "No Reminders"});
+            res.status(201).send({message: "No Reminders"});
         }
     }
     catch(err){
@@ -48,11 +48,27 @@ app.get('/getAllReminders', async (req,res)=>{
     }
 });
 
-app.get('/getReminder', async(req,res)=>{
+app.get('/getAllReminded', async (req,res)=>{
+    try{    
+        const allReminders = await Reminder.find({reminded:{$ne:false}});    
+        if(allReminders.length !==0 ){  
+            res.status(201).send(allReminders);    
+        }else{   
+            res.status(201).send({message: "No Reminders"});
+        }
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).send();
+    }
+});
+
+
+app.get('/getReminderByName', async(req,res)=>{
     try{   
-        const reminder = await Reminder.findOne({reminderMessage: req.body.reminder});
-        if(reminder){            
-            res.status(200).json(reminder);       
+        const reminder = await Reminder.find({reminderMessage: req.body.reminder});
+        if(reminder.length===0){            
+            res.status(201).json(reminder);       
         }else{            
             res.status(400).send("could not find such reminder");
         }
@@ -65,18 +81,13 @@ app.get('/getReminder', async(req,res)=>{
 app.post('/addReminder', async(req,res)=>{
     try{
         const body = req.body;
-        const exist = await Reminder.findOne({reminderMessage: req.body.reminder});
-        if(exist){   
-            res.status(400).send({message: "Already exists "});
-        }else{
-            const reminder = new Reminder({
-                reminderMessage: body.reminder,
-                remindAt: new Date(body.remindAt),
-                reminded:false
-            })
-            const newReminder = await reminder.save();
-            res.status(201).json(newReminder);
-        }       
+        const reminder = new Reminder({
+            reminderMessage: body.reminder,
+            remindAt: new Date(body.remindAt),
+            reminded:false
+        })
+        const newReminder = await reminder.save();
+        res.status(201).json(newReminder);       
     }
     catch(err){
         console.error(err);
@@ -84,7 +95,7 @@ app.post('/addReminder', async(req,res)=>{
     }
 });
 
-app.delete('/:id', async(req,res)=>{
+app.delete('/deleteById/:id', async(req,res)=>{
    try{    
         const reminder = await Reminder.findById(req.params.id);
         if(reminder){
@@ -101,18 +112,17 @@ app.delete('/:id', async(req,res)=>{
 
 setInterval( async() => {
     try{
-        const allReminders = await Reminder.find({});
+        const allReminders = await Reminder.find({reminded: {$ne: true}, remindAt: {$lte: new Date()}});
         if(allReminders.length!==0){
             allReminders.forEach(async (reminder) => {
                 try{
-                    if(!reminder.reminded){
-                        const now = new Date();
-                        if(reminder.remindAt-now < 0){
-                            // code for sending the reminder
-                            console.log("reminded "+reminder.reminderMessage+" at "+now);
-                            await Reminder.findByIdAndUpdate(reminder._id,{reminded: true});
-                        }
-                    }
+                    const Now = new Date();
+
+                    // code for sending the reminder
+
+                    console.log("reminded "+reminder.reminderMessage+" at "+Now);
+                    reminder.reminded = true;
+                    await reminder.save();
                 }catch(err){
                     console.error(err);
                 }         
